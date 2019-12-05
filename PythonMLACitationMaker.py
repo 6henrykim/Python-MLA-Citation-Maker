902bf38d6de9e3571e6b96d29dcef81f3d39da80
6henrykim <6henrykim@gmail.com>
2019-12-04 19:11:22 -0600

    Begin Citation Class
    Started inputAuthors function but it needs to handle the different number of authors and when author doesn't have a middle initial

--- PythonMLACitationMaker.py
+++ PythonMLACitationMaker.py
@@ -1,15 +1,49 @@
-'''
+"""
     Creates MLA citation page based on input from user
-'''
+"""
 
 #import the docx library
 from docx import Document
 
-#open the document
-document = Document()
+
+class Citation:
+    numAuthors = 0
+    authors = []
+    title = ""
+    container = ""
+    contributors = []
+    version = ""
+    publisher = ""
+    location = ""
+    datePublished = ""
+    dateAccessed = ""
+
+    def inputAuthors(self):
+        self.numAuthors = input("How many authors? ")
+
+        
+        lastName = input("Enter last name: ")
+        firstName = input("Enter first name: ")
+        middleInitial = input("Enter middle initial: ")
+        
+        self.authors.append(lastName + ", " + firstName + " " + middleInitial + ".")
+    
+    
+
+#try to open the document or create it if it doesn't exist
+try:
+    document = Document("test.docx")
+except:
+    document = Document()
+
+firstCitation = Citation()
 
 #write heading
-document.add_paragraph("Citations")
+document.add_paragraph("Works Cited")
+
+firstCitation.inputAuthors()
+p=document.add_paragraph(firstCitation.numAuthors)
+p.add_run(firstCitation.authors[0])
 
 #Save the document
 document.save("test.docx")
